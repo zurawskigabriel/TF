5aaca423cab716770e3ad22fae1c0991b3b13175
vinipdiass <quaackyt@gmail.com>
2023-10-18 18:55:56 -0300

    servicoEstoque e Produto

--- Produto.java
+++ Produto.java
@@ -4,11 +4,26 @@ import Acesso_a_Dados.number;
 import Acesso_a_Dados.String;
 
 public class Produto {
-
 	private number codigo;
-
 	private String descricao;
-
 	private number precoUnitario;
+
+	public Produto(number codigo, String descricao, number precoUnitario){
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.precoUnitario = precoUnitario;
+	}
+
+	public number getCodigo(){
+		return codigo;
+	}
+
+	public String getDescricao(){
+		return descricao;
+	}
+
+	public number getPrecoUnitario(){
+		return precoUnitario;
+	}
 
 }
