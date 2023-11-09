c3bcd48f3448e095c7b09278e62f33eac28d21c5
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2023-11-09 18:31:16 -0300

    jpa fix

--- demo1/src/main/java/com/tf/demo1/Demo1Application.java
+++ demo1/src/main/java/com/tf/demo1/Demo1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages={"com.tf"})
+@EnableJpaRepositories(basePackages={"com.tf.demo1"})
 @SpringBootApplication
 public class Demo1Application {
 
