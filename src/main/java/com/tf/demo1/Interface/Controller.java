f5462b94be80d074e5ed2d533127964e3c16bea9
zurawskigabriel <gabrielzds03@gmail.com>
2023-11-20 12:29:15 -0300

    não sei o que aconteceu aqui

--- src/main/java/com/tf/demo1/Interface/Controller.java
+++ src/main/java/com/tf/demo1/Interface/Controller.java
@@ -3,6 +3,9 @@ package com.tf.demo1.Interface;
 import java.util.List;
 
 import com.tf.demo1.Aplicacao.ListarTodosProdutos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import com.tf.demo1.Aplicacao.ProdutosDisponiveis;
 import com.tf.demo1.Aplicacao.SolicitarOrcamento;
 import com.tf.demo1.Dominio.IRepOrcamentos;
@@ -10,11 +13,6 @@ import com.tf.demo1.Dominio.ItemPedido;
 import com.tf.demo1.Dominio.Orcamento;
 import com.tf.demo1.Dominio.Produto;
 import com.tf.demo1.Persistencia.OrcamentoH2BD_ITF;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-
 
 @RestController
 @RequestMapping("/loja")
